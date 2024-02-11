a3cdd117dfaa5be01d57a1726e399fb8029aba84
D Verma <verma.deepak3256@gmail.com>
2024-02-11 19:39:28 +0530

    Delete Royal_hotels directory

--- Royal_hotels/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Hotels,Rooms,Reservation
-# Register your models here.
-admin.site.register(Hotels)
-admin.site.register(Rooms)
-admin.site.register(Reservation)
